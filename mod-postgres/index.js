08269ff25d6ef178f6bd5a916bae833384d599fa
has <hwaravita@woolworths.com.au>
2019-01-03 10:14:45 +1100

    error handling

--- mod-postgres/index.js
+++ mod-postgres/index.js
@@ -5,7 +5,7 @@ const readtime = async()=>{
     let data;
     console.log("f1");
     try{
-        let dbReesult =  await pool.query('SELECT NOW()1');
+        let dbReesult =  await pool.query('SELECT NOW()');
         console.log('XXXXXXXXXXXX' + dbReesult)
         
         if(dbReesult){
@@ -14,23 +14,18 @@ const readtime = async()=>{
         console.log("f2");
     }catch(err){
         console.log("e2");
-        //throw err;
-        //return await Promise.reject("Error with the query");
         return await Promise.reject(new Error("Whoops!"));
     }finally{
         console.log("f3");
         await pool.end();
     }
     
-
-    
     return data;
     
 }
 
 console.log("startig ...");
-let results = readtime().then((resolve, reject)=>{
-    console.log("ERROR",reject);
+let results = readtime().then((resolve)=>{
     console.log("Results found", JSON.stringify(resolve));
 }).catch((err)=>{
     console.log("Catch ERROR",err);
