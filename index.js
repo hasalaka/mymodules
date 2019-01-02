04c7cfb2c977fd3f0d918a5854004192ed1f468b
has <hwaravita@woolworths.com.au>
2019-01-02 11:07:42 +1100

    babelify

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from 'express'
+import { square, diag } from 'hasmath'
+
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    let result = diag(4, 5)
+    res.send('Hello World ' + result)
 })
 
 app.listen(port, () => console.log('App started from $s', port))
